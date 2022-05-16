89ea97b6aa6485d39636290347a9b1715e0d022c
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2022-05-16 18:40:51 +0900

    카카오맵 클릭시 길찾기로 이동, 마커에 텍스트 달기 미완성

--- pointi-homepage/pointi-homepage-react/src/containers/kakao/KakaoMap.jsx
+++ pointi-homepage/pointi-homepage-react/src/containers/kakao/KakaoMap.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -9,31 +10,44 @@ const Map = styled.div`
 `;
 
 const KakaoMap = () => {
+    let map;
+    let marker;
+
     const options = {
         //지도를 생성할 때 필요한 기본 옵션
         center: new window.kakao.maps.LatLng(37.521031834622924, 127.03607157207173), //지도의 중심좌표.
         level: 2, //지도의 레벨(확대, 축소 정도)
-        marker: {
-            position: new window.kakao.maps.LatLng(37.521031834622924, 127.03607157207173),
-            text: 'Point-I 5층건물'
-        }
+        // marker: {
+        //     position: new window.kakao.maps.LatLng(37.521031834622924, 127.03607157207173),
+        //     text: 'Point-I (5층)'
+        // },
     };
 
     const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
 
     useEffect(() => {
-        new window.kakao.maps.StaticMap(container.current, options); //지도 생성 및 객체 리턴
-        return () => {};
+        map = new window.kakao.maps.Map(container.current, options) //지도 생성 및 객체 리턴
+        map.setDraggable(false);
+        map.setZoomable(false);
+        marker = new window.kakao.maps.Marker({
+            position: map.getCenter()
+        })
+        marker.setMap(map)
     }, []);
-
-
+    
+    const findPointI = () => {
+        window.open('https://map.kakao.com/link/to/포인트아이,37.521031834622924,127.03607157207173','포인트아이 길찾기');
+    }
+   
     return (
         <Wrapper>
             <Map
                 className="map"
                 style={{ width: "40vw", height: "70vh" }}
                 ref={container}
+                onClick={findPointI}
             />
+            {/* <a href={'https://map.kakao.com/link/to/포인트아이,37.521031834622924,127.03607157207173'} target='_blank'>test</a> */}
         </Wrapper>
     )
 }
