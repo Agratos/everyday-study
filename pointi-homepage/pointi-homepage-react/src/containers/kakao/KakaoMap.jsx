43d3f8661b7a28a95d86053192cb04276ae64891
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2022-05-27 10:39:26 +0900

    카카오맵 곡선 작성중 // click시 좌표 얻어 오기 성공

--- pointi-homepage/pointi-homepage-react/src/containers/kakao/KakaoMap.jsx
+++ pointi-homepage/pointi-homepage-react/src/containers/kakao/KakaoMap.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import kakaoData from 'assets/dummy/kakao.json';
 
 const Wrapper = styled.div`
     width: inherit;
@@ -12,18 +13,36 @@ const Map = styled.div`
 `;
 
 const KakaoMap = () => {
-    const location = {latitude:37.521031834622924, longitude:127.03607157207173};
+    const location = [
+        {latitude:37.521031834622924, longitude:127.03607157207173},
+        {latitude:35.81820900081049, longitude:127.10553536304494}
+    ];
+    const [linePath, setLinePath] = useState([])
+    const polyline = new window.kakao.maps.Polyline({
+        path: linePath,
+        strokeWeight: 4,
+        strokeColor: 'yellow',
+        strokeOpactiy: 1,
+        strokeStyle: 'solid',
+    })
+    console.log(linePath)
+    const circle = new window.kakao.maps.Circle({
+        center : new window.kakao.maps.LatLng((location[0].latitude + location[1].latitude)/2, (location[0].longitude + location[1].longitude)/2),  // 원의 중심좌표 입니다 
+        radius: 95167, // 미터 단위의 원의 반지름입니다 
+        strokeWeight: 5, // 선의 두께입니다 
+        strokeColor: '#75B8FA', // 선의 색깔입니다
+        strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: 'solid', // 선의 스타일 입니다
+        fillColor: '#CFE7FF', // 채우기 색깔입니다
+        fillOpacity: 0.7  // 채우기 불투명도 입니다   
+    }); 
     let map;
     let marker;
 
     const options = {
         //지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(location.latitude, location.longitude), //지도의 중심좌표.
-        level: 2, //지도의 레벨(확대, 축소 정도)
-        // marker: {
-        //     position: new window.kakao.maps.LatLng(37.521031834622924, 127.03607157207173),
-        //     text: 'Point-I (5층)'
-        // },
+        center: new window.kakao.maps.LatLng(36.7, 127), //지도의 중심좌표.
+        level: 12, //지도의 레벨(확대, 축소 정도)
     };
 
     const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
@@ -31,11 +50,21 @@ const KakaoMap = () => {
     useEffect(() => {
         map = new window.kakao.maps.Map(container.current, options) //지도 생성 및 객체 리턴
         map.setDraggable(false);
-        map.setZoomable(false);
-        marker = new window.kakao.maps.Marker({
-            position: new window.kakao.maps.LatLng(location.latitude, location.longitude)
+        //map.setZoomable(false);
+        location.map(({latitude,longitude}) => {
+            marker = new window.kakao.maps.Marker({
+                position: new window.kakao.maps.LatLng(latitude, longitude)
+
+            })
+            marker.setMap(map);
         })
-        marker.setMap(map)
+        window.kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+            let clickPosition = mouseEvent.latLng;
+            //setLinePath([...linePath,{latitude: clickPosition.getLat(),longitude: clickPosition.getLng()}])
+            console.log(clickPosition.getLat(),clickPosition.getLng())
+        })
+        circle.setMap(map);
+        polyline.setMap(map);
     });
     
     const findPointI = () => {
@@ -47,7 +76,7 @@ const KakaoMap = () => {
             <Map
                 className="map"
                 ref={container}
-                onClick={findPointI}
+                //onClick={findPointI}
             />
         </Wrapper>
     )
